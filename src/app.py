8500ac8fcc65e738c16f68d6214d96207a75fbe0
KarolisKrakys <itskaroliukas@gmail.com>
2022-09-13 11:46:16 -0500

    setup the flask files and docker enviroinment

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from contextlib import redirect_stderr
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_word():
+        return 'Flask'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
