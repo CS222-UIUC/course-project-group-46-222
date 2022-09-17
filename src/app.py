6b8a3b7cc273a6da08c7cee45a66c707ce7340e1
KarolisKrakys <itskaroliukas@gmail.com>
2022-09-17 18:02:53 -0500

    search

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
 from contextlib import redirect_stderr
-from flask import Flask, redirect, url_for, render_template
-
+from flask import Flask, redirect, url_for, render_template, request
+import scrape
 app = Flask(__name__)
 
-@app.route('/')
-
-def hello_word():
-        return 'Hello World!'
+@app.route('/', methods=["POST", "GET"])
+def main():
+        if request.method == "GET":
+                languages = scrape.scrape()
+                return render_template("index.html", languages=languages)  
 
 
 if __name__ == '__main__':
