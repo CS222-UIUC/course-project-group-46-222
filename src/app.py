01540c3ec3458ed924d1a27cfb723647a5d10321
KarolisKrakys <itskaroliukas@gmail.com>
2022-10-21 15:23:19 -0500

    Trying to make linter happy pt1

--- src/app.py
+++ src/app.py
@@ -9,20 +9,21 @@ app.static_folder = 'static'
 
 @app.route('/', methods=["GET"])
 def main():
-        if request.method == "GET":
-                languages = scrape.scrape()
-                return render_template("index.html", languages=languages)  
+    if request.method == "GET":
+        languages = scrape.scrape()
+        return render_template("index.html", languages=languages)
 
 @app.route('/', methods=['POST'])
 def get_class_type():
-        srch_term = request.form.get("type")
-        return redirect(url_for('searchPage',input=srch_term))
+    srch_term = request.form.get("type")
+    return redirect(url_for('searchPage',input=srch_term))
 
 
 @app.route('/searchPage/<input>')
 def searchPage(input=None):
-        df = getClassList.getClassList(input)
-        return df[['Subject', 'Number','Course Title']].rename_axis(None).to_html(header="true", table_id="table",index=False)
+    df = getClassList.getClassList(input)
+    return (df[['Subject', 'Number','Course Title']].rename_axis(None)
+            .to_html(header="true", table_id="table",index=False))
 
 
  
