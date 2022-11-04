7e566ee3059dc92b30bdb0e5bdca835b3149e318
awesomeSarnabh <92769073+awesomeSarnabh@users.noreply.github.com>
2022-11-04 17:38:20 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ app.secret_key = "super secret key"
 def main():
     """"Renders the search box and the background"""
     languages = scrape.scrape()
-
     return render_template("index.html", languages=languages)
 
 @app.route('/', methods=['POST'])
@@ -19,19 +18,24 @@ def get_class_type():
     """"Redirect you to the specific page after you type in the subject type"""
     srch_term = request.form.get("type")
     all_terms = scrape.scrape()
-    if(srch_term in all_terms):
+    if srch_term in all_terms:
         return redirect(url_for('search_page',srch_term=srch_term))
-    else:
-        return redirect(url_for('get_class_type'))
+    return redirect(url_for('get_class_type'))
 
-@app.route('/searchPage/<srch_term>')
+@app.route('/searchPage/<srch_term>/')
 def search_page(srch_term):
     """"Displays the classes and the GPA after receiving input"""
-    languages = scrape.scrape()
     data_frame = get_class_list.get_class_list(srch_term)
-    return render_template("searchPage.html", data_table = data_frame[['Subject', 'Number','Course Title']].rename_axis(None)
-            .to_html(header="true", table_id="table",index=False), srch_term = srch_term, languages=languages)
-            
+    languages = scrape.scrape()
+    data_frame = data_frame[['Subject', 'Number','Course Title']].rename_axis(None)
+    data_frame['Course Title'] = (data_frame['Course Title']
+                                  .apply(lambda x: f'<a href="{x}">{x}</a>'))
+    return render_template("searchPage.html", data_table = data_frame.to_html(header="true", table_id="table",index=False, escape=False), srch_term = srch_term, languages=languages)
+
+@app.route('/searchPage/<srch_term>/<class_num>')
+def look_up_class(srch_term, class_num):
+    """"Actually shows all of the GPAs and the info we have about a certain class such as CS 124"""
+    return render_template('displayData.html', class1=class_num)
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
