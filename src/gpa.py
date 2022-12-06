5d6cc0450b9e77c6333257a8d8f0644d65269c87
Anay Sharma <anays2@illinois.edu>
2022-12-05 20:34:00 -0600

    'graphs'

--- src/gpa.py
+++ src/gpa.py
@@ -50,8 +50,62 @@ def createData(number, name, term):
         tmp.append(year)
         tmp.append(instructor)
         tmp.append(avg_gpa)
+        tmp.append(num_students)
         data.append(tmp)
 
     #print(data)    
-    df_classyear = pd.DataFrame(data, columns = ['YearTerm', 'Instructor', 'AvgGPA'])
+    df_classyear = pd.DataFrame(data, columns = ['YearTerm', 'Instructor', 'AvgGPA','totalStudents'])
     return df_classyear
+
+def createImages(number, name, year):
+    name = name.upper()
+    new_df = df.loc[(df['Number'] == number)  & (df['Subject'] == name)]
+    data = []
+    yrs = []
+    ins = []
+    gpas = []
+    studs = []
+    paths = []
+    years = new_df.YearTerm.unique()
+    df_year = new_df.loc[(new_df['YearTerm'] == year)]
+    instructors = df_year.Primary_Instructor.unique()
+    df_year
+    i = 0
+    for instructor in instructors:
+        df_instructor = df_year.loc[(df_year['Primary_Instructor']) == instructor]
+        new = (df_instructor.sum(axis = 0, skipna = True))
+
+        num_students = [new['A+']+new['A'], new['A-'], new['B+'], new['B'], new['B-'], new['C+'], new['C'], new['C-'], new['D+'], new['D'], new['D-'], new['F']]
+        
+        gpa = [4.00, 3.67, 3.33, 3.00, 2.67, 2.33, 2.00, 1.67, 1.33, 1.00, 0.67, 0.00]
+        total = sum(num_students, 0)
+    
+        weighted_sum = []
+        for j in range(len(gpa)):
+            weighted_sum.append(num_students[j]*gpa[j])
+        avg_gpa = sum(weighted_sum) / total
+        As = num_students[0] / total
+
+        print("{}  {}  | {} | {}  | {}  ".format(year,instructor,(avg_gpa),total,num_students))
+        yrs.append(year)
+        ins.append(instructor)
+        gpas.append(avg_gpa)
+        studs.append(num_students)
+        tmp = []
+        tmp.append(year)
+        tmp.append(instructor)
+        tmp.append(avg_gpa)
+        tmp.append(total)
+        data.append(tmp)
+        #linegraph(gpa, num_students, instructor)
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(gpa, num_students, color='tab:blue')
+        ax.set_title(instructor)
+        plt.savefig('static/image{}.jpg'.format(i))
+        i += 1
+        paths.append('static/image{}.jpg'.format(i))
+
+
+    #print(data)    
+    return paths
